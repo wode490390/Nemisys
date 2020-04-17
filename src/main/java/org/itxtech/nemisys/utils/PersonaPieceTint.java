c8736fe3e5df400594a5eddc1bcf445cefa7635f
Wodor <17339354+wode490390@users.noreply.github.com>
2020-04-17 20:49:46 +0800

    Update to 1.14.60

---
+++ src/main/java/org/itxtech/nemisys/utils/PersonaPieceTint.java
@@ -0,0 +1,17 @@
+package org.itxtech.nemisys.utils;
+
+import com.google.common.collect.ImmutableList;
+import lombok.ToString;
+
+import java.util.List;
+
+@ToString
+public class PersonaPieceTint {
+    public final String pieceType;
+    public final ImmutableList<String> colors;
+
+    public PersonaPieceTint(String pieceType, List<String> colors) {
+        this.pieceType = pieceType;
+        this.colors = ImmutableList.copyOf(colors);
+    }
+}
